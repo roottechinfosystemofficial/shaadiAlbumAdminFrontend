de25fa6bd9b0f77fbdfcf987f477b551872f679e
roottechinfosystemofficial <roottechinfosystem@gmail.com>
2025-04-25 16:37:22 +0530

    a

--- backend/controller/photosS3.controller.js
+++ backend/controller/photosS3.controller.js
@@ -135,7 +135,7 @@ export const getAppEventImages = async (req, res) => {
     return res.status(400).json({ error: "Missing eventId" });
   }
 
-  const prefix = `eventimages/${eventId}/${subEventId}/`;
+  const prefix = `eventimages/${eventId}/680b55e432fff6cc637292e8/`;
 
   let allItems = [];
   let continuationToken;
