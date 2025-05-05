ae5b68054d055748024b6568d2b235a7d8df4cab
bagalepa <bagalepa@gmail.com>
2025-05-05 19:22:22 +0530

    feat: Update Flipbookfun and event folder components for improved layout and mobile responsiveness

--- App/src/Screens/SelectionEventFolders.jsx
+++ App/src/Screens/SelectionEventFolders.jsx
@@ -43,7 +43,7 @@ const SelectionEventFolders = () => {
     <ScreenWrapper bg="white">
       <StatusBar
         barStyle={"dark-content"} // icon/text color
-        backgroundColor={Platform.OS === "android" ? "#000" : "transparent"}
+        backgroundColor={"transparent"}
         translucent
       />
       <View style={styles.container}>
