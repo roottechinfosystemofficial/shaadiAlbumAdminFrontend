6ca703b176e6f29a041501c8ab74d1ccc80ddee4
yashsuthar <yash.suthar9898@gmail.com>
2025-06-07 12:45:40 +0530

    delete confirom by priyanc on call

--- albumapp/babel.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  presets: ["babel-preset-expo"],
-  plugins: [
-    // Only Babel plugins should be listed here
-    "react-native-reanimated/plugin", // must be last
-  ],
-};
-
